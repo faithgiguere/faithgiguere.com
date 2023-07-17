cd9968080dde78f1c29a5505aed3234e51c6845c
faithmayer <faithnmayer@gmail.com>
2023-07-16 21:16:57 -0400

    initial website refresh: removed outdated information, updated images and about section

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-aws s3 sync ./ s3://faithgiguere.com/ --delete --exclude "venv*" --exclude ".git*" --acl public-read
+aws s3 sync ./ s3://faithmayer.com/ --delete --exclude "venv*" --exclude ".git*" --acl public-read
